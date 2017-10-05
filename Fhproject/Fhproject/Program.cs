e28b43fc9a53fcf1ac263caaa89a77c4c2c0d7b7
joharc <johann.archan@brainformance.at>
2017-10-05 20:25:00 +0200

    Firstproject

---
+++ Fhproject/Fhproject/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fhproject
+{
+    class Program
+    {
+        static void Main(string[] args) {
+            Console.WriteLine("Yo es funkt!");
+            Console.ReadLine();
+        }
+    }
+}
